675b115eca4ddaf1eab559ec90067f16fed5ea1e
G I <tronnie@Gs-MacBook-Pro.local>
2017-08-22 21:02:42 -0700

    pulling and loading gifs according to button name yay

--- assets/logic.js
+++ assets/logic.js
@@ -1,28 +1,51 @@
 console.log("hola! we are documents are connected")
 
-$("#search").on('click', function(){
-  console.log('the click is on!')
-});
+
 
 
 
 //GLOBAL VARIABLES
-var instrument = ("");
-var queryURL =  "https://api.giphy.com/v1/gifs/search?api_key=d6ed00ce3a7b4d4cb205f3d20fe6750c&q=" + instrument;
+// var instrument = ("");
+// var queryURL =  "https://api.giphy.com/v1/gifs/search?api_key=d6ed00ce3a7b4d4cb205f3d20fe6750c&limit=10&q=" + instrument;
 
 //FUNCTIONS
   //various instruments
 
+
     //-accordion
-    $("#accordion").on('click', function(){
+    $("button").on('click', function(){
             console.log('the accordion is on!')
+
+      var animal = $(this).attr("data-instrument");
+      var queryURL =  "https://api.giphy.com/v1/gifs/search?api_key=d6ed00ce3a7b4d4cb205f3d20fe6750c&limit=10&q=" + animal;
+
+
       $.ajax ({
-              url:"https://api.giphy.com/v1/gifs/search?api_key=d6ed00ce3a7b4d4cb205f3d20fe6750c&q=accordion", 
+              url:queryURL,
               method: "GET"})
-       .done(function (response){
+       .done(function (response) {
+            console.log(queryURL);
             console.log(response);
+            console.log(response.data);
+
+          var results = response.data;
+          for (var i=0; i<results.length; i++){
+            var animalDiv = $("<div>");
+            var p = $("<p>").text("Rating: " + results[i].rating);
+            var image = $("<img>");
+            image.attr("src", results[i].images.fixed_height.url);
+
+            animalDiv.append(p);
+            animalDiv.append(image);
+
+            $(".instruments").prepend(animalDiv);
+          }
        });
     });
+
+    // $("#search").on('click', function(){
+    //   console.log('the click is on!')
+    // });
     //-cowbell
     //-maracas
     //-tamborine
