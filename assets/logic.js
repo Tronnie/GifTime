d8297c5c002773f4d1048d74e16cf2ed0b4a601e
G I <tronnie@Gs-MacBook-Pro.local>
2017-08-22 22:42:29 -0700

    working on input form. bugs on animate

--- assets/logic.js
+++ assets/logic.js
@@ -1,4 +1,4 @@
-console.log("hola! we are documents are connected")
+console.log("hola! html and JS are connected")
 
 
 
@@ -17,12 +17,13 @@ console.log("hola! we are documents are connected")
             console.log('the accordion is on!')
 
       var animal = $(this).attr("data-instrument");
+      // var state =$(this).attr("data-state")
       var queryURL =  "https://api.giphy.com/v1/gifs/search?api_key=d6ed00ce3a7b4d4cb205f3d20fe6750c&limit=10&q=" + animal;
 
 
       $.ajax ({
               url:queryURL,
-              method: "GET"})
+              method: "GET"})//<--ajax
        .done(function (response) {
             console.log(queryURL);
             console.log(response);
@@ -33,15 +34,35 @@ console.log("hola! we are documents are connected")
             var animalDiv = $("<div>");
             var p = $("<p>").text("Rating: " + results[i].rating);
             var image = $("<img>");
-            image.attr("src", results[i].images.fixed_height.url);
+            image.attr("src", results[i].images.fixed_height_still.url);
 
             animalDiv.append(p);
             animalDiv.append(image);
 
             $(".instruments").prepend(animalDiv);
-          }
-       });
-    });
+
+            $(image).on('click', function(){
+              console.log ("image clicked");
+              var animate = $("<img>");
+              image.attr("src", results[i].images.fixed_height.url);
+            })
+
+          }//<--for loop
+
+          $("#add-instrument").on("click", function(event){
+            console.log ("submit clicked");
+            event.preventDefault();
+            var newInst = $("#instrument-input").val().trim();
+            results.push(newInst);
+
+          })//<--input form
+
+       }); //<--.done
+
+    }); //<--button onclick
+
+
+
 
     // $("#search").on('click', function(){
     //   console.log('the click is on!')
