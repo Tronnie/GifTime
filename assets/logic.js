49d43b87a042cde6427fb11bdd9d77f7b496f6a6
G I <tronnie@Gs-MacBook-Pro.local>
2017-08-25 11:17:53 -0700

    ratings censor is on;need buttons;need no prepend

--- assets/logic.js
+++ assets/logic.js
@@ -1,8 +1,10 @@
 console.log("hola! html and JS are connected")
 
+// //Test. 12 displayMovieIn
+// function displayInstButton() {
+
     //-accordion
     $("button").on('click', function(){
-            console.log('the accordion is on!')
 
       var inst = $(this).attr("data-instrument");
       // var state =$(this).attr("data-state")
@@ -14,20 +16,21 @@ console.log("hola! html and JS are connected")
 
        .done(function (response) {
             console.log(response.data);
-
           var results = response.data;
           for (var i=0; i<results.length; i++){
-            var instDiv = $("<div>");
-            var p = $("<p>").text("Rating: " + results[i].rating);
+            //Test 11. appropriate Rating
+            if (results[i].rating !== "r" && results[i].rating !== "pg-13"){
+              var instDiv = $("<div>");
+              var p = $("<p>").text("Rating: " + results[i].rating);
 
 
             //Test 7. add a movingImage loop in order to animate all gifs!
-    function animateImage(){
-        //Test 3. works! but only on first gif
-        $(instDiv).on('click', function(){
-          console.log ("tickle me image!");
-          instDiv.html(animate);
-        }); //<--click image
+            function animateImage(){
+                //Test 3. works! but only on first gif
+                $(instDiv).on('click', function(){
+                  console.log ("tickle me image!");
+                  instDiv.html(animate);
+                }); //<--click image
 
                 //Original
             var image = $("<img>");
@@ -40,15 +43,63 @@ console.log("hola! html and JS are connected")
             instDiv.append(image);
             // instDiv.append(animate);
             $(".instruments").prepend(instDiv);
+
+            }    //<--rating
     } //<--animateImage function
     animateImage ()  //<--Test 7.
 
-
-
         }//<--for loop
-
-
 
        }); //<--.done
 
+
+
+
+    // Function for displaying movie data
+          function renderButtons() {
+
+            // Deleting the movies prior to adding new movies
+            // (this is necessary otherwise you will have repeat buttons)
+            $("#buttons-view").empty();
+
+            // Looping through the array of movies
+            for (var i = 0; i < results.length; i++) {
+
+              // Then dynamicaly generating buttons for each movie in the array
+              // This code $("<button>") is all jQuery needs to create the beginning and end tag. (<button></button>)
+              var a = $("<button>");
+              // Adding a class of movie to our button
+              a.addClass("btns");
+              // Adding a data-attribute
+              a.attr("data-instrument", results[i]);
+              // Providing the initial button text
+              a.text(results[i]);
+              // Adding the button to the buttons-view div
+              $("#buttons-view").append(a);
+            }
+          }
+
+          // This function handles events where a movie button is clicked
+          $("#add-instrument").on("click", function(event) {
+            event.preventDefault();
+            console.log("submit clicked");
+            // This line grabs the input from the textbox
+            var newInst = $("#instrument-input").val().trim();
+
+            // Adding movie from the textbox to our array
+            results.push(newInst);
+
+            // Calling renderButtons which handles the processing of our movie array
+            renderButtons();
+          });
+
+
+  // Adding a click event listener to all elements with a class of "movie"
+  // $(document).on("click", ".newInst", displayInstButton);
+
+  // Calling the renderButtons function to display the intial buttons
+  renderButtons();
+
     }); //<--button onclick
+
+  // }  //<--Test.12 displayInstButton
