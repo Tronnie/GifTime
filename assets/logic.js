f06994b03bb8894df1084833b625779830dff9f0
G I <tronnie@Gs-MacBook-Pro.local>
2017-08-26 18:04:19 -0700

    bugs!!!ugh

--- assets/logic.js
+++ assets/logic.js
@@ -17,25 +17,26 @@ console.log("hola! html and JS are connected")
        .done(function (response) {
             console.log(response.data);
           var results = response.data;
+
           for (var i=0; i<results.length; i++){
             //Test 11. appropriate Rating
             if (results[i].rating !== "r" && results[i].rating !== "pg-13"){
               var instDiv = $("<div>");
               var p = $("<p>").text("Rating: " + results[i].rating);
 
-
             //Test 7. add a movingImage loop in order to animate all gifs!
             function animateImage(){
                 //Test 3. works! but only on first gif
                 $(instDiv).on('click', function(){
-                  console.log ("tickle me image!");
+                  console.log ("tickled image!");
                   instDiv.html(animate);
                 }); //<--click image
 
                 //Original
             var image = $("<img>");
             image.attr("src", results[i].images.fixed_height_still.url);
-            //
+  ///HOw to toggle?
+
             var animate =$("<img>")
             animate.attr("src", results[i].images.fixed_height.url);
 
@@ -44,36 +45,37 @@ console.log("hola! html and JS are connected")
             // instDiv.append(animate);
             $(".instruments").prepend(instDiv);
 
+  ///need to create a way that it animates the gif clicked
+
             }    //<--rating
     } //<--animateImage function
     animateImage ()  //<--Test 7.
 
         }//<--for loop
 
-       }); //<--.done
+      //  }); //<--.done
 
 
 
 
-    // Function for displaying movie data
+    // Function for displaying  data
           function renderButtons() {
 
-            // Deleting the movies prior to adding new movies
+            // Deleting
             // (this is necessary otherwise you will have repeat buttons)
             $("#buttons-view").empty();
 
             // Looping through the array of movies
-            for (var i = 0; i < results.length; i++) {
+            for (var j = 0; j < results.length; j++) {
 
-              // Then dynamicaly generating buttons for each movie in the array
-              // This code $("<button>") is all jQuery needs to create the beginning and end tag. (<button></button>)
+              // Then dynamicaly generating buttons
               var a = $("<button>");
               // Adding a class of movie to our button
               a.addClass("btns");
               // Adding a data-attribute
-              a.attr("data-instrument", results[i]);
+              a.attr("data-instrument", results[j]);
               // Providing the initial button text
-              a.text(results[i]);
+              a.text(results[j]);
               // Adding the button to the buttons-view div
               $("#buttons-view").append(a);
             }
@@ -86,19 +88,15 @@ console.log("hola! html and JS are connected")
             // This line grabs the input from the textbox
             var newInst = $("#instrument-input").val().trim();
 
-            // Adding movie from the textbox to our array
+            // Adding from the textbox to our array
             results.push(newInst);
 
             // Calling renderButtons which handles the processing of our movie array
             renderButtons();
           });
+// }//<--for loop
+      }); //<--.done
 
-
-  // Adding a click event listener to all elements with a class of "movie"
-  // $(document).on("click", ".newInst", displayInstButton);
-
-  // Calling the renderButtons function to display the intial buttons
-  renderButtons();
 
     }); //<--button onclick
 
